f56e932e6653cdf48c195293d4e765a3abd777b2
Harun <mdharunbd88@gmail.com>
2021-10-23 13:20:27 +0600

    app js update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import "./scss/app.scss";
-import { StepBackwardOutlined, StepForwardFilled } from "@ant-design/icons";
-const feather = require("feather-icons");
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <StepBackwardOutlined />
-        <StepForwardFilled />
-      </header>
+      <header className="App-header">Project run successfully</header>
     </div>
   );
 }
