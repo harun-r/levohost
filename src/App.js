4051198b3879507db78804e18a22148abe81e72b
Harun <>
2021-10-29 10:36:24 +0600

    Hero Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./Components/Header/Header";
 import "./assets/scss/app.scss";
+import HeroHome from "./Components/HeroHome/HeroHome";
 
 function App() {
   return (
     <div className="app-wrapper">
-      <Header />
+        <Header />
+        <HeroHome/>
     </div>
   );
 }
