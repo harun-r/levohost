4a728eefa32effe0cf3b5a5a0f2aba327edf9bb2
Harun <mdharunbd88@gmail.com>
2021-11-13 21:12:52 +0600

    affiliate

--- src/pages/Affiliate.js
+++ src/pages/Affiliate.js
@@ -1,14 +1,10 @@
 import React, {Fragment} from 'react';
 import Header from "../Components/Header/Header";
 import HeaderSmall from "../Components/HeaderSmall/HeaderSmall";
-import AboutLevo from "../Components/AboutUs/AboutLevo";
-import OurHistory from "../Components/AboutUs/OurHistory";
-import OurVisionMission from "../Components/AboutUs/OurVisionMission";
-import OurTeam from "../Components/OurTeam/OurTeam";
-import WhatSection from "../Components/SharedComponent/WhatSection/WhatSection";
 import Reviews from "../Components/Reviews/Reviews";
 import Footer from "../Components/Footer/Footer";
 import HowAffiliateWork from "../Components/HowAffiliateWork/HowAffiliateWork";
+import AffiliateCommission from "../Components/AffiliateCommission/AffiliateCommission";
 
 const Affiliate = () => {
     return (
@@ -19,13 +15,8 @@ const Affiliate = () => {
                 desc="Get commissions each time you refer a new customer to LevoHost!"
             />
             <HowAffiliateWork/>
-            <OurHistory/>
-            <OurVisionMission/>
-            <OurTeam/>
-            <WhatSection
-                spcStyle="text-white bg-dark"
-                btnName="light"
-            />
+            <AffiliateCommission/>
+
             <Reviews/>
             <Footer/>
         </Fragment>
